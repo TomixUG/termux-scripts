a0a04c1e243d274afdb229a8b89d744e2bd739be
TomixUG <tomas.sousek1@gmail.com>
2024-06-16 02:36:28 +0200

    absolute path

--- music.sh
+++ music.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # spotify
-spotdl sync "https://open.spotify.com/playlist/4vGLQg0jj1d5H4xltzSxs0?si=01be5263d7b948b7" --save-file playlist.spotdl --output ~/storage/music/spotdl/mymusic
+spotdl sync "https://open.spotify.com/playlist/4vGLQg0jj1d5H4xltzSxs0?si=01be5263d7b948b7" --save-file ~/termux-scripts/playlist.spotdl --output ~/storage/music/spotdl/mymusic
 
 #yutube
  yt-dlp --embed-thumbnail --download-archive archive.txt -x --audio-format mp3 --output "~/storage/music/spotdl/mymusic/%(title)s.%(ext)s" "https://youtube.com/playlist?list=PLegDNYyCBYSCmiyTYww3GVPVjhz5GUnMK"
